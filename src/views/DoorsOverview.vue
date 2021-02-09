26ed4c790ab6515ad86feb97a713a6e7df901564
DieterClaessens67 <dieterclaessens3@hotmail.com>
2021-02-09 10:09:36 +0100

    temp

--- src/views/DoorsOverview.vue
+++ src/views/DoorsOverview.vue
@@ -7,11 +7,31 @@
 
 <script>
 import Doors from '@/components/Doors'
+import https from 'https'
 
 export default {
   name: 'DoorsOverview',
   components: {
     Doors
+  },
+  created(){
+    let data = {
+      "User": {
+        "login_id": "admin",
+        "password": "dIET34#ucll"
+      }
+  }
+    //jquery.post("http://localhost:8080/api/login",JSON.stringify(data))
+    const httpsAgent = new https.Agent({
+      rejectUnauthorized: false
+    })
+    fetch("https://localhost:443/api/login", {
+      headers: "Access-Control-Allow-Origin",
+      method: "POST",
+      body: JSON.stringify(data),
+      agent: httpsAgent
+    }).then(response => console.log(response.headers))
+      .catch(err => console.log(err.response))
   }
 }
 </script>
