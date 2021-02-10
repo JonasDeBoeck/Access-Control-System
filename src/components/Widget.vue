dbeac171fff490ba9a99c0640d4e6411045816ea
RafaelBackx <rafaelbackx@live.be>
2021-02-10 13:44:46 +0100

    working on widgets overview

---
+++ src/components/Widget.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="widget" :style="cssVars">
+        <h2>{{widget.name}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Widget",
+    props: ["widget"],
+    computed: {
+        cssVars(){
+            return {
+                '--background': this.widget.colour
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .widget {
+        background-color: var(--background);
+        width: 100%;
+        height: 100%;
+        border-radius: .35rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        box-shadow: 0 .15rem 1.75rem 0 rgba(58, 59, 69, .15);
+    }
+</style>
--- src/views/WidgetsOverview.vue
+++ src/views/WidgetsOverview.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="widgets">
     <h1>Widgets</h1>
-    <AddWidget/>
+    <div class="content">
+      <div class="widgets">
+        <Widget v-for="widget in widgets" v-bind:key="widget.name" v-bind:widget="widget"/>
+      </div>
+      <AddWidget class="form" v-on:add-widget="addWidget"/>
+    </div>
   </div>
 </template>
 
 <script>
 import * as f from '../variables'
 import AddWidget from '../components/AddWidget.vue'
+import Widget from '../components/Widget.vue'
 export default {
   name: 'WidgetsOverview',
   components:{
-    AddWidget
+    AddWidget,
+    Widget
   },
   created() {
     f.default.login("admin","t",f.default.getDoors)
@@ -32,9 +39,84 @@ export default {
             id: 3
         }
         ],
-      selectedDoor: undefined
+      selectedDoor: undefined,
+      widgets: [
+        {
+          colour: '#03fcdb',
+          name: "test Widget",
+          doors: [
+            {
+              name: "K2.09",
+              id: 1
+            },
+            {
+              name: "K2.10",
+              id: 2
+            }],
+            duration: {
+              hours: 0,
+              minutes: 1,
+              seconds: 30
+            }
+        },
+        {
+          colour: '#db96eb',
+          name: "open slagboom 90s",
+          doors: [
+            {
+              name: "slagboom",
+              id: 1
+            },
+            ],
+            duration: {
+              hours: 0,
+              minutes: 1,
+              seconds: 30
+            }
+        },
+        {
+          colour: '#a7eba2',
+          name: "open leeraarskamer 90s",
+          doors: [
+            {
+              name: "k0.1",
+              id: 1
+            },
+            ],
+            duration: {
+              hours: 0,
+              minutes: 1,
+              seconds: 30
+            }
+        }
+      ]
+      }
+    },
+    methods:{
+      addWidget(widget){
+        this.widgets.push(widget)
       }
     }
 }
 </script>
+
+<style scoped>
+  .content{
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .widgets{
+    background: #F8F9FC;
+    display: grid;
+    grid-template-rows: 2fr 2fr 2fr;
+    grid-template-columns: 1fr 1fr;
+    row-gap: 20%;
+    column-gap: 5%;
+    margin-left: 2.5em;
+    margin-right: 2.5em;
+    margin-top: 2.5em;
+    height: 80%;
+  }
+</style>
 
