89dd68878cf6e3d000a1ee6d54c6c54d5e0b0ad6
JonasDeBoeck <Jonas.deboeck@student.ucll.be>
2021-02-12 14:59:53 +0100

    Toasts dooroverview

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import vueSelect from 'vue'
 import Multiselect from 'vue-multiselect'
 import Modal from "@burhanahmeed/vue-modal-2";
 import VueSession from 'vue-session'
+import Toasted from 'vue-toasted';
 
 // persist so that the session id is stored between tabs
 var options = {
@@ -16,6 +17,7 @@ Vue.config.productionTip = false
 Vue.component("v-select", vueSelect)
 Vue.component('multiselect', Multiselect)
 Vue.use(Modal);
+Vue.use(Toasted)
 new Vue({
   router,
   render: h => h(App)
