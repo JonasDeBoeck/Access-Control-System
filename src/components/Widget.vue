d28f63695a27deb53efdb71c18f8076b7bc3bd5d
Thibault Magnini <thibault.magnini@student.ucll.be>
2021-02-12 10:40:05 +0100

    beetje extra design widget pagina

--- src/components/Widget.vue
+++ src/components/Widget.vue
@@ -50,10 +50,8 @@ export default {
 
 <style scoped>
     .widget {
-        /* border-left: 5px solid;
-        border-bottom: 5px solid; */
-        /* padding: 10px; */
-        border: 5px solid;
+       
+        border: 3px solid;
         border-color: var(--background);
         width: 100%;
         height: 100%;
@@ -62,6 +60,7 @@ export default {
         align-items: center;
         justify-content: space-between;
         box-shadow: 0 .15rem 1.5rem 0 rgba(58, 59, 69, .5);
+        margin-left: 2em;
     }
 
     .iconcontainer{
@@ -71,6 +70,10 @@ export default {
         height: 100%;
         flex-basis: 20%;
         background: var(--background);
+        border-top-left-radius: 1em;
+        border-bottom-left-radius: 1em;
+        padding: 0.7em;
+        box-shadow: 0 .15rem 1.5rem 0 rgba(58, 59, 69, .5);
     }
 
     .content{
@@ -80,6 +83,8 @@ export default {
         flex-direction: column;
         justify-content: space-between;
         margin-right: 1em;
+        font-family: 'Oswald';
+        margin-left: 1.5em;
     }
 
     .link{
@@ -103,7 +108,15 @@ export default {
         /* border-radius: 10px; */
         /* margin-bottom: 1em; */
         border: none;
+        box-shadow: 0 .15rem 1.5rem 0 rgba(58, 59, 69, .5);
+        margin-right: 1em;
     }
+
+    .button:hover {
+        transform: translateY(-3px);
+        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+    }
+
     .divbutton{
         margin: 10px;
     }
@@ -112,5 +125,9 @@ export default {
         display:flex;
         flex-direction: column;
         align-items: space-evenly;
+    }
+
+    h2 {
+        color:  rgba(58,96,208,1);
     }
 </style>
--- src/views/WidgetsOverview.vue
+++ src/views/WidgetsOverview.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="widgets">
-    <h1>Widgets</h1>
+    <div class="head">
+      <h1>Widgets</h1>
+      <form action="">
+        <input type="text" placeholder="Zoek op..." v-model="searchterm" @input="search">
+        <button type="submit">
+          <i class="fas fa-search fa-sm">
+          </i>
+        </button>
+      </form>
+    </div>
     <div class="content">
       <div class="widgets">
         <Widget v-for="widget in widgets" v-bind:key="widget.name" v-bind:widget="widget" v-on:del-widget="deleteWidget"/>
@@ -133,5 +142,48 @@ export default {
     margin-top: 2.5em;
     height: 80%;
   }
+
+  .head {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-left: 2.5em;
+    padding-right: 2.5em;
+    padding-top: 1em;
+    padding-bottom: 1em;
+    background: #fff;
+    box-shadow: 0 .15rem 1.75rem 0 rgba(58, 59, 69, .15);
+  }
+
+  input[type=text] {
+    border: 0;
+    color: #6E707E;
+    background-color: rgba(58, 59, 69, .15);
+    padding:.50rem 1rem;
+    border-top-left-radius: .35rem;
+    border-bottom-left-radius: .35rem;
+  }
+  input:focus, button:focus {
+  outline: none;
+}
+button:hover {
+  background: rgb(26, 72, 207);
+}
+button {
+  margin-left: -1px;
+  color: #fff;
+  background: rgba(58,96,208,1);
+  border-color: rgba(58,96,208,1);
+  border: 0px;
+  border-top-right-radius: .35rem;
+  border-bottom-right-radius: .35rem;
+  padding:.50rem 1rem;
+}
+h1 {
+  color: rgba(58,96,208,1);
+  margin: 0;
+  font-family: 'Oswald';
+  text-transform: uppercase;
+}
 </style>
 
